10df9412f7cdbe01fd1abce3ad8bf46d4d20f230
AndreiKurilov <gipertorroid@gmail.com>
2021-07-30 16:46:51 +0300

    feat(Header): first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import h from './Header.module.css';
+
+const Header = () => {
+  return <header className={h.header}>
+    <img src = "https://i.pinimg.com/474x/d3/0a/19/d30a19c7f5c494218deb98202f3f6422.jpg" alt = 'logo'/>
+  </header>
+}
+
+export default Header;
