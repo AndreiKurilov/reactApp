e9e7f97deeaff7a8b2e2c37507f00777ba465516
AndreiKurilov <gipertorroid@gmail.com>
2021-07-30 16:49:25 +0300

    feat: first commit (News)

---
+++ src/components/News/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import n from './News.module.css';
+
+const News = () => {
+  return (
+    <div className={n.n}>
+      News
+    </div>
+  )
+}
+
+export default News;
