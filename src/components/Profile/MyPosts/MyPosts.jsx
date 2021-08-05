08549d76acf4b46bc9e5a3a2c7eb00e96bdf48df
AndreiKurilov <gipertorroid@gmail.com>
2021-08-05 22:20:15 +0300

    feat(Profile): add ref for the button

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,15 +7,22 @@ const MyPosts = (props) => {
   let postsElement = props.state.posts.map(p => <Post message={p.message} likesCount={p.likesCount}
   /> )
 
+  let newPostElement = React.createRef();
+  console.log('newPostElement:', newPostElement)
+  
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text)
+  }
   return (
     <div className={c.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add</button>
+          <button onClick={ addPost }>Add</button>
         </div>
       </div>
       <div className={c.posts}>
