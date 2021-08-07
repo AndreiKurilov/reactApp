f1293bc5a1c55a86530a777d2aa3488b77d8c438
AndreiKurilov <gipertorroid@gmail.com>
2021-08-07 18:45:01 +0300

    feat(App): add FLUX

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ const App = (props) => {
         <Header />
         <Navbar />
         <div className="app-wrapper-content">
-          <Route path="/dialogs" render={ () => 
-                                 <Dialogs state={props.state.dialogsPage} />} />
-          <Route path="/profile" render={ () => 
-                                 <Profile state={props.state.profilePage}
-                                          addPost={props.addPost} />} />
+          <Route path="/dialogs" render={ () => <Dialogs 
+                 dialogsPage={props.state.dialogsPage}
+                 addMessage={props.addMessage}
+                 updateNewMessageText={props.updateNewMessageText} />} />
+          <Route path="/profile" render={ () => <Profile 
+                 profilePage={props.state.profilePage}
+                 addPost={props.addPost}
+                 updateNewPostText={props.updateNewPostText} />} />
           <Route path="/news" component={News} />
         </div>
       </div>
