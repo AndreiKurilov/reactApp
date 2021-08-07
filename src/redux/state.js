42ca7b453c050d2b2ce70418e70f3099f1cf351d
AndreiKurilov <gipertorroid@gmail.com>
2021-08-07 18:42:43 +0300

    feat(Redux): add FLUX

--- src/redux/state.js
+++ src/redux/state.js
@@ -5,7 +5,8 @@ let state = {
     posts: [
       { id: 1, message: "Hi, how are you?", likesCount: 3 },
       { id: 2, message: "It's my first post.", likesCount: 21 },
-    ]
+    ],
+    newPostText: 'abcde'
   },
   dialogsPage: {
     dialogs: [
@@ -22,18 +23,40 @@ let state = {
       { id: 3, message: "Yo" },
       { id: 4, message: "Yo" },
       { id: 5, message: "Yo" }
-    ]
+    ],
+    newMessageText: 'Enter something'
   }
 }
 
-export let addPost = (postMess) => {
+export let addPost = () => {
   let newPost = { 
     id: 3, 
-    message: postMess, 
+    message: state.profilePage.newPostText, 
     likesCount: 0 
   };
   state.profilePage.posts.push(newPost);
+  state.profilePage.newPostText = '';
   rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+  state.profilePage.newPostText = newText;
+  rerenderEntireTree(state);
+}
+
+export let addMessage = () => {
+  let newMessage = {
+    id: 6,
+    message: state.dialogsPage.newMessageText
+  };
+  state.dialogsPage.messages.push(newMessage);
+  state.dialogsPage.newMessageText = '';
+  rerenderEntireTree(state);
+}
+
+export let updateNewMessageText = (newText) => {
+  state.dialogsPage.newMessageText = newText;
+  rerenderEntireTree(state)
 }
 
 export default state;
