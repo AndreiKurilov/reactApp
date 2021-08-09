19054d211fb708476e850922902afe7694fe06c9
AndreiKurilov <gipertorroid@gmail.com>
2021-08-09 18:24:24 +0300

    feat(Redux): add observer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+  console.log("State changed")
+}
 
 let state = {
   profilePage: {
@@ -28,7 +30,7 @@ let state = {
   }
 }
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = { 
     id: 3, 
     message: state.profilePage.newPostText, 
@@ -39,12 +41,12 @@ export let addPost = () => {
   rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
 }
 
-export let addMessage = () => {
+export const addMessage = () => {
   let newMessage = {
     id: 6,
     message: state.dialogsPage.newMessageText
@@ -54,9 +56,15 @@ export let addMessage = () => {
   rerenderEntireTree(state);
 }
 
-export let updateNewMessageText = (newText) => {
+export const updateNewMessageText = (newText) => {
   state.dialogsPage.newMessageText = newText;
-  rerenderEntireTree(state)
+  rerenderEntireTree(state);
 }
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
+}
+
+
 
 export default state;
