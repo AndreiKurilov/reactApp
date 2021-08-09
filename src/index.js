a3f2a624b84bc85cad06d35e10f7d19258f15d28
AndreiKurilov <gipertorroid@gmail.com>
2021-08-09 18:28:07 +0300

    feat(App): transfer data from render.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import state from './redux/state';
-import {rerenderEntireTree} from './render'
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { addMessage, addPost, updateNewMessageText, updateNewPostText } from './redux/state';
+import { BrowserRouter } from 'react-router-dom';
 
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App state={state} 
+           addPost={addPost} 
+           updateNewPostText={updateNewPostText}
+           addMessage={addMessage}
+           updateNewMessageText={updateNewMessageText} />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
