7904747f319fcd0b5679758663f22cadee6ab15c
AndreiKurilov <gipertorroid@gmail.com>
2021-08-10 20:35:06 +0300

    refactor(Redux): transfer data to a single object

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,70 +1,75 @@
-let rerenderEntireTree = () => {
-  console.log("State changed")
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likesCount: 3 },
-      { id: 2, message: "It's my first post.", likesCount: 21 },
-    ],
-    newPostText: 'abcde'
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 3 },
+        { id: 2, message: "It's my first post.", likesCount: 21 },
+      ],
+      newPostText: 'abcde'
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "Andrey" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Viktor" },
+        { id: 6, name: "Valera" }
+      ],
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" }
+      ],
+      newMessageText: 'Enter something'
+    }
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Dimych" },
-      { id: 2, name: "Andrey" },
-      { id: 3, name: "Sveta" },
-      { id: 4, name: "Sasha" },
-      { id: 5, name: "Viktor" },
-      { id: 6, name: "Valera" }
-    ],
-    messages: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "Yo" },
-      { id: 4, message: "Yo" },
-      { id: 5, message: "Yo" }
-    ],
-    newMessageText: 'Enter something'
+  
+  getState () {
+    return this._state;
+  },
+
+  _callSubscriber() {
+    console.log("State changed")
+  },
+  
+  addPost () {
+    debugger
+    let newPost = { 
+      id: 3, 
+      message: this._state.profilePage.newPostText, 
+      likesCount: 0 
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  
+  updateNewPostText (newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  
+  addMessage () {
+    let newMessage = {
+      id: 6,
+      message: this._state.dialogsPage.newMessageText
+    };
+    this._state.dialogsPage.messages.push(newMessage);
+    this._state.dialogsPage.newMessageText = '';
+    this._callSubscriber(this._state);
+  },
+  
+  updateNewMessageText (newText) {
+    this._state.dialogsPage.newMessageText = newText;
+    this._callSubscriber(this._state);
+  },
+  
+  subscribe (observer) {
+    this._callSubscriber = observer;
   }
 }
 
-export const addPost = () => {
-  let newPost = { 
-    id: 3, 
-    message: state.profilePage.newPostText, 
-    likesCount: 0 
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-export const addMessage = () => {
-  let newMessage = {
-    id: 6,
-    message: state.dialogsPage.newMessageText
-  };
-  state.dialogsPage.messages.push(newMessage);
-  state.dialogsPage.newMessageText = '';
-  rerenderEntireTree(state);
-}
-
-export const updateNewMessageText = (newText) => {
-  state.dialogsPage.newMessageText = newText;
-  rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-
-
-export default state;
+export default store;
