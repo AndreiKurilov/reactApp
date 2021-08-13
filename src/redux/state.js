53ab91803241d616e2dd5b873e5a315e31ded249
AndreiKurilov <gipertorroid@gmail.com>
2021-08-13 17:10:35 +0300

    refactor(Redux): make action.type into a const

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,9 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT ='UPDATE-NEW-POST-TEXT';
+const ADD_MESSAGE = 'ADD-MESSAGE';
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
+
+
 let store = {
   _state: {
     profilePage: {
@@ -68,5 +74,16 @@ let store = {
     }
   }
 }
+
+export const addPostActionCreator = () => ({ type: ADD_POST })
+
+export const updateNewPostTextActionCreator = (text) => 
+({ type: UPDATE_NEW_POST_TEXT, newText: text })
+
+export const addMessageActionCreator = () => ({ type: ADD_MESSAGE})
+
+export const updateNewMessageTextActionCreator = (text) => 
+({ type: UPDATE_NEW_MESSAGE_TEXT, newText: text })
+
 
 export default store;
