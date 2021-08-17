9a6fe54004157a22da08bc251bb7ea9275577911
AndreiKurilov <gipertorroid@gmail.com>
2021-08-17 18:51:31 +0300

    feat(App): change the file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import { BrowserRouter, Route } from "react-router-dom";
 import News from "./components/News/News";
 
 const App = (props) => {
@@ -14,13 +14,11 @@ const App = (props) => {
         <Header />
         <Navbar />
         <div className="app-wrapper-content">
-          <Route path="/dialogs" render={ () => <Dialogs 
-                 store={props.store}
-                 />} />
-          <Route path="/profile" render={ () => <Profile 
-                 profilePage={props.state.profilePage}
-                 dispatch={props.dispatch}
-                 />} />
+          <Route path="/dialogs" 
+                 render={ () => <Dialogs store={props.store} />} />
+          <Route path="/profile" 
+                 render={ () => <Profile profilePage={props.state.profilePage}
+                                         dispatch={props.dispatch} />} />
           <Route path="/news" component={News} />
         </div>
       </div>
