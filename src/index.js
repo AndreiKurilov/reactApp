a6fb0fdd05c4be882b54e97fa9432ff0c155bf46
AndreiKurilov <gipertorroid@gmail.com>
2021-08-19 17:31:06 +0300

    refactor(App): change the file structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import store from './redux/redux-store';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import store from './redux/redux-store';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
