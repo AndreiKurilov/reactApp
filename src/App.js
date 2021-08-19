134f1b3a0b07867944fdcb26f0efc4811b85cec3
AndreiKurilov <gipertorroid@gmail.com>
2021-08-19 17:28:48 +0300

    refactor(App): change the file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
-import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 const App = (props) => {
   return (
@@ -15,10 +15,9 @@ const App = (props) => {
         <Navbar />
         <div className="app-wrapper-content">
           <Route path="/dialogs" 
-                 render={ () => <Dialogs store={props.store} />} />
+                 render={ () => <DialogsContainer store={props.store} />} />
           <Route path="/profile" 
-                 render={ () => <Profile profilePage={props.state.profilePage}
-                                         dispatch={props.dispatch} />} />
+                 render={ () => <Profile store={props.store} />} />
           <Route path="/news" component={News} />
         </div>
       </div>
