02989851b7176c05dda71b8b2b4f4c5d3e5c5f79
AndreiKurilov <gipertorroid@gmail.com>
2021-08-24 18:11:12 +0300

    feat(App): delete the rerender function

--- src/index.js
+++ src/index.js
@@ -5,21 +5,12 @@ import { Provider } from 'react-redux';
 import './index.css';
 import store from './redux/redux-store';
 import App from './App';
-
-let rerenderEntireTree = () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>,
-    document.getElementById('root')
-  );
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
+  
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
