b31efb83843d23fdfb65ad91025eeedd41c1d0ba
AndreiKurilov <gipertorroid@gmail.com>
2021-08-28 17:19:53 +0300

    refactor(Users): transfer users from users-reducer

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import us from './Users.module.css'
+import us from './Users.module.css';
+// import * as axios from 'axios'; 
 
 let Users = (props) => {
+  debugger
+  if (props.users.length === 0) {
 
-  // if (props.users.length === 0) {
-  //   props.setUsers( [
-  //     { id: 1, photoUrl: 'https://avatarko.ru/img/kartinka/1/Shrek.jpg', 
-  //       followed: false, fullName: 'Andrew', status: 'I am the boss', 
-  //       location: {city: 'Moscow', country: 'Russia'} 
-  //     },
-  //     { id: 2, photoUrl: 'https://avatarko.ru/img/kartinka/1/Shrek.jpg', 
-  //       followed: true, fullName: 'Sasha', status: 'I am not the boss', 
-  //       location: {city: 'Minsk', country: 'Belarus'} 
-  //     },
-  //     { id: 3, photoUrl: 'https://avatarko.ru/img/kartinka/1/Shrek.jpg', 
-  //       followed: false, fullName: 'Mikola', status: 'I am the ???', 
-  //       location: {city: 'Kiev', country: 'Ukrain'} 
-  //     },
-  //     ]
-  //   )
-  // }
+    // axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    //   props.setUsers()
+    // })
+
+    props.setUsers( [
+      { id: 1, photoUrl: 'https://avatarko.ru/img/kartinka/1/Shrek.jpg', 
+        followed: false, fullName: 'Andrew', status: 'I am the boss', 
+        location: {city: 'Moscow', country: 'Russia'} 
+      },
+      { id: 2, photoUrl: 'https://avatarko.ru/img/kartinka/1/Shrek.jpg', 
+        followed: true, fullName: 'Sasha', status: 'I am not the boss', 
+        location: {city: 'Minsk', country: 'Belarus'} 
+      },
+      { id: 3, photoUrl: 'https://avatarko.ru/img/kartinka/1/Shrek.jpg', 
+        followed: false, fullName: 'Mikola', status: 'I am the ???', 
+        location: {city: 'Kiev', country: 'Ukrain'} 
+      },
+      ]
+    )
+  }
 
   return <div>
     {
