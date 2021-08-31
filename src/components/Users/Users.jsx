d5a9a41e208ad544d04eccace0ff4c3965e13911
AndreiKurilov <gipertorroid@gmail.com>
2021-08-31 12:24:29 +0300

    feat(Users): add pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,17 +4,28 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/userPhoto.png'
 
 class Users extends React.Component {
-
-  constructor(props) {
-    super(props);
-
+  componentDidMount() {
     axios.get('https://social-network.samuraijs.com/api/1.0/users')
     .then(response => { this.props.setUsers(response.data.items);
     })
   }
 
   render() {
+
+    let pagesCount = Math.ceil(this.props.totalUsersCount/this.props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
     return <div>
+      <div>
+        {pages.map(p => {
+          return <span className={ this.props.currentPage === p && us.selectedPage }>{p}</span>
+          })
+        }
+      </div>
     {
       this.props.users.map(u => <div key={u.id}>
         <div>
