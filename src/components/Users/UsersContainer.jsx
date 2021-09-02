28b747e476226c2b5bb83540ba157044afe576fe
AndreiKurilov <gipertorroid@gmail.com>
2021-09-02 18:51:28 +0300

    refactor(Users): change the file structure

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -22,8 +22,13 @@ class UsersContainer extends React.Component {
   }
 
   render() {
-    return <Users totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize} currentPage={this.props.currentPage} onPageChanged={this.onPageChanged} 
-    users={this.props.users} follow={this.props.follow} unfollow={this.props.unfollow} />
+    return <Users totalUsersCount={this.props.totalUsersCount} 
+            pageSize={this.props.pageSize} 
+            currentPage={this.props.currentPage} 
+            onPageChanged={this.onPageChanged} 
+            users={this.props.users} 
+            follow={this.props.follow} 
+            unfollow={this.props.unfollow} />
   }
 }
 
