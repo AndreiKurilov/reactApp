054ebef3a68930134424190e9259b1b00913193f
AndreiKurilov <gipertorroid@gmail.com>
2021-09-04 09:43:08 +0300

    refactor(Users): add Preloader component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -3,8 +3,7 @@ import * as axios from 'axios';
 import Users from './Users';
 import { connect } from 'react-redux';
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC, toggleIsFetchingtAC } from '../../redux/users-reducer';
-import preloader from '../../assets/images/preloader.gif';
-import us from './Users.module.css';
+import Preloader from '../common/Preloader/Preloader';
 
 class UsersContainer extends React.Component {
   componentDidMount() {
@@ -29,7 +28,7 @@ class UsersContainer extends React.Component {
 
   render() {
     return <>
-      { this.props.isFetching ? <img src={preloader} alt='preloader' className={us.preloaderStyle}/> : null }
+      { this.props.isFetching ? <Preloader /> : null }
       <Users totalUsersCount={this.props.totalUsersCount} 
               pageSize={this.props.pageSize} 
               currentPage={this.props.currentPage} 
