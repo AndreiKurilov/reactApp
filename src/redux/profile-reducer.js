259aa0e39235d7f3a7a5a5ecb63f174fb8d8c515
AndreiKurilov <gipertorroid@gmail.com>
2021-09-06 18:23:41 +0300

    feat(Redux): add action creator

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT ='UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE ='SET_USER_PROFILE';
 
 let initialState = {
   posts: [
@@ -7,6 +8,7 @@ let initialState = {
     { id: 2, message: "It's my first post.", likesCount: 21 },
   ],
   newPostText: 'abcde',
+  profile: null
 }
 
 const profileReducer = (state = initialState, action) => {
@@ -20,13 +22,19 @@ const profileReducer = (state = initialState, action) => {
       return {
         ...state,
       posts: [...state.posts, newPost ],
-      newPostText: '',
+      newPostText: ''
       }
     }
     case UPDATE_NEW_POST_TEXT: {
       return {
         ...state,
-        newPostText: action.newText,
+        newPostText: action.newText
+      }
+    }
+    case SET_USER_PROFILE: {
+      return {
+        ...state,
+        profile: action.profile
       }
     }
     default:
@@ -38,5 +46,8 @@ export const addPostActionCreator = () => ({ type: ADD_POST })
 
 export const updateNewPostTextActionCreator = (text) => 
 ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+
+export const setUserProfile = (profile) => 
+({ type: SET_USER_PROFILE, profile })
 
 export default profileReducer;
