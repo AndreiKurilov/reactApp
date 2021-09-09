8643992bf98a4b546db386fb048e1d87c6f5f144
AndreiKurilov <gipertorroid@gmail.com>
2021-09-09 11:28:04 +0300

    feat(Header): create login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import h from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return <header className={h.header}>
     <img src = "https://i.pinimg.com/474x/d3/0a/19/d30a19c7f5c494218deb98202f3f6422.jpg" alt = 'logo'/>
+
+    <div className={h.loginBlock}>
+      <NavLink to={'/login'}>Login</NavLink>
+    </div>
   </header>
 }
 
