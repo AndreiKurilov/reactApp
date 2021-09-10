ce0bebde897920f206a1448becbde2ad045fef1b
AndreiKurilov <gipertorroid@gmail.com>
2021-09-10 11:57:43 +0300

    feat(Header): add props

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import h from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
   return <header className={h.header}>
     <img src = "https://i.pinimg.com/474x/d3/0a/19/d30a19c7f5c494218deb98202f3f6422.jpg" alt = 'logo'/>
 
     <div className={h.loginBlock}>
-      <NavLink to={'/login'}>Login</NavLink>
+      { props.isAuth ? props.login
+       : <NavLink to={'/login'}>Login</NavLink>
+      }
     </div>
   </header>
 }
