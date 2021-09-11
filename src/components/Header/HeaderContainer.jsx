b2f95a73a559d09fda1e380f71458b1b2918c1e7
AndreiKurilov <gipertorroid@gmail.com>
2021-09-11 11:42:21 +0300

    refactor(Header): change the file structure

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -6,10 +6,10 @@ import { setAuthUserData } from './../../redux/auth-reducer';
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, { withCredentials: true})
+    axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, 
+    { withCredentials: true })
     .then(response => {
       if (response.data.resultCode === 0) {
-        debugger
         let {id, email, login} = response.data.data;
         this.props.setAuthUserData(id, email, login);
       }
